47bfad8f85b66934b59891aef4eea8123d23e199
Nicolas Ramos <nicolandres1.nr@gmail.com>
2024-08-02 14:31:41 -0500

    correction vite main

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ app.mount('#app')
 
 import { createApp, markRaw } from 'vue';
 import { createPinia } from 'pinia';
-import router from './router';
+import router from './src/router';
 
 /* import './localAxios'; */
-import './style.css';
-import App from './App.vue';
+import './src/style.css';
+import App from './src/App.vue';
 
 const pinia = createPinia();
 
--- vite.config.js
+++ vite.config.js
@@ -23,5 +23,5 @@ export default defineConfig({
   server:{
     port:3000
   },
-  base: '/src/'
+  base: '/'
 })
