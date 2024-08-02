f6d85c46c1b41ae1d8826e0fa4bf3fbba0f6b26b
Nicolas Ramos <nicolandres1.nr@gmail.com>
2024-08-02 14:05:43 -0500

    main outside ignore

---
+++ main.js
@@ -0,0 +1,34 @@
+/* import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
+ */
+
+import { createApp, markRaw } from 'vue';
+import { createPinia } from 'pinia';
+import router from './src/router';
+
+/* import './localAxios'; */
+import './style.css';
+import App from './App.vue';
+
+const pinia = createPinia();
+
+pinia.use(({ store })=>{
+    store.router = markRaw(router)
+    console.log(router) 
+}) 
+
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+
+app.mount('#app');
