82f035abc2d117dc22a13c81a19d8496d647c4c8
Nicolas Ramos <nicolandres1.nr@gmail.com>
2024-08-02 15:00:21 -0500

    main.js correction

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-/* import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-
-const app = createApp(App)
-
-app.use(createPinia())
-
-app.mount('#app')
- */
-
 import { createApp, markRaw } from 'vue';
 import { createPinia } from 'pinia';
 import router from './router';
