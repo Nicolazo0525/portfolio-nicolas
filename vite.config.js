431de2e452138266c1c425f0a25544888f749392
Nicolas Ramos <nicolandres1.nr@gmail.com>
2024-08-06 10:59:31 -0500

    root router vite config

--- vite.config.js
+++ vite.config.js
@@ -14,16 +14,16 @@ export default defineConfig({
     }
   }
 }) */
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-/* export default defineConfig({
+https://vitejs.dev/config/
+ export default defineConfig({
   plugins: [vue()],
   server:{
     port:3000
   }
-}) */
+})  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
