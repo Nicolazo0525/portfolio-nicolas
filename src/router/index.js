2210365f4fd8a1fdb2aded9984fdb6ce37cccaf3
Nicolas Ramos <nicolandres1.nr@gmail.com>
2024-08-06 10:53:08 -0500

    root router

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Home from '../components/Home.vue';
 
 const routes = [
     {
-        path: "/",
+        path: "/portfolio-nicolas/",
         name: "Home",
         component: Home
     }
--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,11 @@ export default defineConfig({
     }
   }
 }) */
-/* import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/* export default defineConfig({
   plugins: [vue()],
   server:{
     port:3000
