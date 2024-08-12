570dc673d50e990d01c3418e910eb98aa50a7836
Nicolas Ramos <nicolandres1.nr@gmail.com>
2024-08-12 15:52:29 -0500

    composition API sTmout

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     }
   }
 }) */
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 //https://vitejs.dev/config/
@@ -23,12 +23,12 @@ import vue from '@vitejs/plugin-vue'
   server:{
     port:3000
   }
-}) 
-/* import { defineConfig } from 'vite'
+})  */
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: 'https://nicolazo0525.github.io/portfolio-nicolas/',
-}) */
+})
