6fe95291bdd7404a044b6423754488ef6014e519
Nicolas Ramos <nicolandres1.nr@gmail.com>
2024-09-27 17:17:10 -0500

    Correction error deploy. localaxios coment in main instance ->'Netlify: Details corrections and added content'

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp, markRaw } from 'vue';
 import { createPinia } from 'pinia';
 import router from './router';
 
-/* import './localAxios'; */
+import './localAxios';
 import './style.css';
 import App from './App.vue';
 
